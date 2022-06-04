e9f2f95cee1b1c1dc8c4cb7a54e883fee0ebda0e
Nadya Trofimova <nadia.trf1@gmail.com>
2022-06-04 21:28:21 +0300

    db logic

--- src/entrypoint.php
+++ src/entrypoint.php
@@ -5,7 +5,6 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
-use PDO;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 (new Entrypoint())->main();
@@ -15,27 +14,57 @@ class Entrypoint {
         Dotenv::createImmutable(dirname(__DIR__))->load();
         $app = AppFactory::create();
         $app->add(array($this, 'authenticate'));
-        $pdo = new PDO(
-            dsn: "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}",
-            username: $_ENV['DB_USERNAME'],
-            password: $_ENV['DB_PASSWORD'],
-            options: array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            )
-        );
 
         $app->any('/ping', function(Request $request, Response $response) {
             $response->getBody()->write('meow');
             return $response;
         });
 
-        $app->post('/admin/initDatabase', function(Request $request, Response $response){
-            if (!$request->getAttribute('admin')) return $this->response($response, ['status' => 'нет подключения'],403);
-            return $this->response($response, ['status' => 'OK'],201);
-        });
+//        $app->post('/admin/initDatabase', function(Request $request, Response $response){
+//            if (!$request->getAttribute('admin')) return $this->response($response, ['status' => 'нет подключения'],403);
+//            return $this->response($response, ['status' => 'OK'],201);
+//        });
+
+        $tables = [
+            'accommodation',
+            'city',
+            'customer',
+            'excursion',
+            'travel',
+            'voucher',
+            'voucher_accommodation',
+            'voucher_excursion',
+            'voucher_travel'
+        ];
+
+        foreach ($tables as $table) {
+            $dbEditor = new DbEditor($table);
+            $app->any("/$table/getAll", function(Request $request, Response $response) use ($dbEditor) {
+                return $this->response($response, $dbEditor->getAll());
+            });
+
+            $app->post("/$table/create", function(Request $request, Response $response) use ($dbEditor) {
+                $dbEditor->create($this->getPostParams($request));
+                return $this->response($response);
+            });
+
+            $app->post("/$table/update", function(Request $request, Response $response) use ($dbEditor) {
+                $dbEditor->update($this->getGetParams($request)['id'], $this->getPostParams($request));
+                return $this->response($response);
+            });
+
+            $app->post("/$table/delete", function(Request $request, Response $response) use ($dbEditor) {
+                $dbEditor->delete($this->getGetParams($request)['id']);
+                return $this->response($response);
+            });
+        }
+
+
         $app->run();
     }
+
+
+
     protected function response(Response $r, $body = array(), $code = 200): Response {
         $r->getBody()->write(json_encode($body));
         $r = $r->withHeader('Content-Type', 'application/json');
@@ -63,7 +92,5 @@ class Entrypoint {
         return $handler->handle($request);
 
     }
-
-
 }
 
---
+++ src/tables/ITable.php
@@ -0,0 +1,69 @@
+<?php
+
+class DbEditor {
+    public PDO $pdo;
+
+    function __construct(
+        public string $table,
+    ) {
+        $this->pdo = new PDO(
+            dsn: "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}",
+            username: $_ENV['DB_USERNAME'],
+            password: $_ENV['DB_PASSWORD'],
+            options: array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            )
+        );
+    }
+
+
+    function create(array $fields): void {
+        $columns = '';
+        foreach ($fields as $key => $value) {
+            $columns .= ' ' . $key . ',';
+        }
+        $columns = substr($columns, 0, strlen($columns) - 1);
+
+        $values = '';
+        foreach ($fields as $key => $value) {
+            $values .= ' ' . $value . ',';
+        }
+        $values = substr($values, 0, strlen($values) - 1);
+
+        $sql = "insert into $this->table ($columns) values ($values);";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+    }
+
+    function getAll(): array {
+        $stmt = $this->pdo->prepare('select * from ' . $this->table);
+        $stmt->execute();
+        $res = [];
+        foreach ($stmt as $row) {
+            $res[] = $row;
+        }
+        return $res;
+    }
+
+    function update(int $id, array $fields): void {
+        $str = [];
+        foreach ($fields as $column => $value)
+        {
+            $str[] = $column . ' = ' . $value . '';
+        }
+        $str = implode(',',$str);
+        $sql = "update $this->table set
+                    $str
+                    where `id` = $id;
+        ";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+    }
+
+    function delete(int $id): void {
+        $stmt = $this->pdo->prepare("delete from $this->table where `id` = :id");
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+}
